ef58ec54ad5e02debd3d0f5f30900ad3a0c34dac
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-10-04 04:46:56 +0100

    create table medical_histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,13 @@ name varchar(200),
 date_of_birth date
 );
 
+CREATE TABLE IF NOT EXISTS medical_histories (
+id SERIAL PRIMARY KEY,
+admitted_at timestamp,
+patient_id int,
+status varchar(200),
+CONSTRAINT fk_patient
+FOREIGN KEY (patient_id)
+REFERENCES patients(id)
+);
+
