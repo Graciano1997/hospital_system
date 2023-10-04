f060b14f2dc0f9800cf7e7d6521d32869d68beaa
coker2019 <olabisi2k11@gmail.com>
2023-10-04 09:21:35 +0100

    Add indexes on invoice.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -62,6 +62,7 @@ CREATE INDEX idx_fk_patient ON medical_histories(patient_id);
 
 CREATE INDEX idx_fk_treatment ON medical_histories_link_treatments(treatment_id)
 
+
 CREATE INDEX idx_fk_history ON invoices(medical_history_id);
 
 CREATE INDEX idx_fk_invoice ON invoice_items(invoice_id);
